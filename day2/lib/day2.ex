ba47c3680d8cce5af6a6b0f62d27692645a81877
c-cero <fedor@tuzovskiy.ru>
2018-12-07 16:15:54 +0100

    4*

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -1,40 +1,38 @@
 defmodule Day2 do
   def start do
-    {:ok, content} = File.read("priv/input.txt")
+    {:ok, content} = File.read("priv/input")
     content
     |> String.split
-    |> Enum.map(&counter/1)
-    |> IO.inspect
-    |> Enum.reduce({0,0}, fn x, acc -> checksum(x, acc) end)
-    |> (fn {x, y} -> x*y end).()
+    |> Enum.map(&String.graphemes/1)
+    |> check_words
   end
 
-  def checksum({true, true}, {a2, a3}), do: {a2+1, a3+1}
-  def checksum({true, false}, {a2, a3}), do: {a2+1, a3}
-  def checksum({false, true}, {a2, a3}), do: {a2, a3+1}
-  def checksum({false, false}, {a2, a3}), do: {a2, a3}
+  def check_words([h|t]) do
+    case check_word(t, h) do
+      {:mismatch, _} -> check_words(t)
+      {:match, word} -> word |> Enum.join()
+    end
+  end
 
-  def counter(list) do
-    list
-    |> String.graphemes
-    |> Enum.sort
-    |> (&(counter(tl(&1), hd(&1), 1, %{two: 0,three: 0}))).()
-  end
-  def counter([], _, count, acc) do
-    case count do
-      2 -> {true, acc.three > 0}
-      3 -> {acc.two > 0, true}
-      _ -> {acc.two > 0, acc.three > 0}
+  def check_word([], _), do: {:mismatch, nil}
+  def check_word([next|t], word) do
+    case check_letter(next, word, false, []) do
+      {:mismatch, _} -> check_word(t, word)
+      {:match, result} -> {:match, result}
     end
   end
-  def counter([h|t], letter, count, acc) when h != letter do
-    case count do
-      2 -> counter(t, h, 1, %{acc | two: acc.two + 1})
-      3 -> counter(t, h, 1, %{acc | three: acc.three + 1})
-      _ -> counter(t, h, 1, acc)
-    end
+  def check_letter([a|b], [c|d], false, acc) when a == c do
+    check_letter(b,d, false, acc ++ [a])
   end
-  def counter([h|t], letter, count, acc) do
-    counter(t, h, count + 1, acc)
+  def check_letter([a|b], [c|d], true, acc) when a == c do
+    check_letter(b,d, true, acc ++ [a])
   end
+  def check_letter([a|b], [c|d], false, acc) when a != c do
+    check_letter(b,d, true, acc)
+  end
+  def check_letter([a|b], [c|d], true, acc) when a != c do
+    {:mismatch, nil}
+  end
+  def check_letter([],[], true, acc), do: {:match, acc}
+
 end
